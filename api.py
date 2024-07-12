1dd8b8fb52b3f88e0f28152eca341fa8bfb03a13
ovaomnis <ovaomnis@gmail.com>
2024-07-12 22:27:33 +0600

    something

--- api.py
+++ api.py
@@ -1,15 +1,46 @@
+from datetime import datetime
+from typing import List
 from decouple import config
 import requests
 from dataclasses import dataclass
+from data_types import fetchUnitsResponse
 
 @dataclass
-class fetchUnitsResponse:
-    realmName: str
+class Round:
+    duration: int
+    endAt: datetime
+    name: str
+    repeat: int
+    startAt: datetime
+    status: str
+
+@dataclass
+class fetchRoundsResponse:
+    gameName: str
+    now: datetime
+    rounds: List[Round]
+
+
+def get_rounds():
+    api_host = get_api_host()
+    token = get_token()
+    res = requests.get(f"{api_host}rounds/zombidef", headers={"X-Auth-Token": token})
+
+    if res.status_code == 200:
+        data = fetchRoundsResponse(**res.json())
+        print(data.rounds)
+    else:
+        print(f"something went wrong..")
+        try:
+            print(res.json())
+        except:
+            print(res.status_code)
 
 
 def get_api_host() -> str:
     api_host = config("API_HOST", default='https://games.datsteam.dev/')
     return api_host
+
 
 def get_token() -> str:
     token = config("TOKEN", default='sdklfj;askdjflkjsad;f')
@@ -34,4 +65,4 @@ def fetchUnits():
 
     
 if __name__ == "__main__":
-    fetchUnits()
+    get_rounds()
