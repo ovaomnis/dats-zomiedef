3f082fae695a116c044bd6af28e7adff05ec0247
ovaomnis <ovaomnis@gmail.com>
2024-07-12 22:07:58 +0600

    something

--- api.py
+++ api.py
@@ -1,4 +1,29 @@
 from decouple import config
+import requests
+import json
+from dataclasses import dataclass
+
+@dataclass
+class fetchUnitsResponse:
+    realmName: str
+
+
+def get_api_host() -> str:
+    api_host = config("API_HOST", default='https://games.datsteam.dev/')
+    return api_host
 
 def fetchUnits():
-    pass
+    api_host = get_api_host()
+    res = requests.get(api_host)
+
+    if res.status_code == 200:
+        data: fetchUnitsResponse = res.json()
+        print(data.realmName)
+    else:
+        print(f"something went wrong...\n{res.status_code}")
+    
+
+    
+    
+if __name__ == "__main__":
+    fetchUnits()
